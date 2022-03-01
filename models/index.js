1d27f3ab61f54051da9c4a2927d36216da466cca
ChotanSharma <chotansharma@yahoo.com>
2022-03-01 06:58:50 -0500

    add the vote

--- models/index.js
+++ models/index.js
@@ -1,14 +1,35 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id',
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
 });
   
 
-module.exports = { User, Post };
+module.exports = { User, Post,  };
